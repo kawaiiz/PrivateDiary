ff6a9af4b3d6f972560f9ca7e1870c21441ab5f5
kawaiiz <690902636@qq.com>
2019-06-26 11:05:59 +0800

    修改上传组件

--- miniprogram/public/components/up_img/up_img.js
+++ miniprogram/public/components/up_img/up_img.js
@@ -7,15 +7,24 @@ Component({
    * 组件的属性列表
    */
   properties: {
+    //存储图片的返回信息
+    valueList: {
+      type: Array,
+      value: [],
+      observer: 'changeImgArr'
+    },
     disabled: {
       type: Boolean,
       value: false,
       observer: 'changeDisabled'
     },
-    //存储图片  [{path:图片路径}]
-    imageList: {
+    imgList: { //存储图片
       type: Array,
       value: []
+    },
+    maxLength: {
+      type: Number,
+      value: 1000000
     }
   },
 
@@ -24,49 +33,23 @@ Component({
    */
   data: {
     baseURL: app.globalData.baseURL,
-    imageURL: app.globalData.imageURL,
-    valueList:[]
-  },
-  observers:{
-    'valueList':function () {
-      this.triggerEvent('setImg', {
-        imageList: this.data.valueList
-      })
-    },
-    'imageList.**':function () {
-      let valueList = this.data.imageList.map(item=>{
-        return item.path
-      })
-      this.setData({
-        valueList
-      })
-    }
+    imageURL: app.globalData.imageURL
   },
   lifetimes: {
-    // attached: function () {
-    //   // 在组件实例进入页面节点树时执行
-    //   debugger
-    // },
-    // detached: function() {
-    //   // 在组件实例被从页面节点树移除时执行
-    //   this.setData({
-    //     imageList: [],
-    //     valueList: []
-    //   })
-    // },
-    // error(err){
-    //   console.log(err)
-    // }
-  },
-  attached: function () {
+    attached: function () {
+      // 在组件实例进入页面节点树时执行
 
-  },
-  detached: function () {
-    // 在组件实例被从页面节点树移除时执行
-    // this.setData({
-    //   imageList: [],
-    //   valueList: []
-    // })
+    },
+    detached: function() {
+      // 在组件实例被从页面节点树移除时执行
+      this.setData({
+        imgList: [],
+        valueList: []
+      })
+    },
+    error(err){
+      console.log(err)
+    }
   },
   pageLifetimes: {
     show: function () {
@@ -83,33 +66,38 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
     setImg() {
       let _this = this
       wx.chooseImage({
         count: 9, // 默认9
-        sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
+        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
         sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
         success: function (res) {
           // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-          let imageList = _this.data.imageList;
+          let imgList = _this.data.imgList;
           let seeImg = _this.data.seeImg;
-          _this.setData({
-            flag: true,
-            disabled:true
-          })
+
+          if (imgList.length + res.tempFilePaths.length > _this.data.maxLength) {
+            util.toast(`最多上传${_this.data.maxLength}张照片`)
+            return false
+          }
+
           wx.showLoading({
             title: '请等待',
           })
-          let j = 0;
+
           let arr = []
+          let j = 0;
           for (let i = 0; i < res.tempFilePaths.length; i++) {
-            arr[i] = _this.upImg(res.tempFilePaths[i], i + _this.data.valueList.length)
+            arr.push(_this.upImg(res.tempFilePaths[i], i + _this.data.valueList.length))
           }
-          Promise.all(arr).then(()=>{
-            _this.setData({
-              disabled:false
-            })
+          console.log(arr)
+          Promise.all(arr).then(res =>{
+            console.log(_this.data.imgList)
+            console.log(_this.data.valueList)
+            wx.hideLoading()
+          }).catch(err=>{
+            console.log(err)
             wx.hideLoading()
           })
         },
@@ -121,11 +109,12 @@ Component({
     //上传图片
     upImg(tempFilePaths, i) {
       let _this = this
-      let del = 'imageList[' + i + '].del'
-      let error = 'imageList[' + i + '].error'
-      let imgStr = 'imageList[' + i + '].path'
-      let x = Math.ceil(Math.random()*1000)
-      return new Promise(resolve => {
+      return new Promise((resolve, reject) => {
+        let del = 'imgList[' + i + '].del'
+        let error = 'imgList[' + i + '].error'
+        let str = 'valueList[' + i + ']'
+        let imgStr = 'imgList[' + i + '].path'
+        let x = Math.random()
         wx.cloud.uploadFile({
           cloudPath: `images/${x}${(new Date()).getTime()}`,
           filePath: tempFilePaths, // 文件路径
@@ -134,6 +123,7 @@ Component({
             if(res.statusCode === 200){
               _this.setData({
                 [imgStr]: res.fileID,
+                [str]:  res.fileID,
                 [del]: true,
                 [error]: false
               })
@@ -151,49 +141,44 @@ Component({
         })
       })
     },
-    //点击查看图片
+    // 预览图片
     seeImg(e) {
-      let imageList = this.data.imageList
+      let imgList = this.data.imgList
       let index = e.currentTarget.dataset.index
+      console.log(imgList)
       let showList = []
-      for (let i = 0; i < imageList.length; i++) {
-        showList.push(app.globalData.baseURL + imageList[i].path)
+      for (let i = 0; i < imgList.length; i++) {
+        showList.push(app.globalData.baseURL + imgList[i].path)
       }
       wx.previewImage({
         current: showList[index], // 当前显示图片的http链接
         urls: showList // 需要预览的图片http链接列表
       })
     },
-    //删除图片
     delImg(e) {
       let index = e.currentTarget.dataset.index
-      let imageList = this.data.imageList
+      let imgList = this.data.imgList
       let valueList = util.deepCopy(this.data.valueList)
-      if(!imageList[index].error){
-        valueList.splice(index, 1)
-      }
-      imageList.splice(index, 1)
-
+      imgList.splice(e.currentTarget.dataset.index, 1)
+      valueList.splice(e.currentTarget.dataset.index, 1)
       this.setData({
-        imageList,
+        imgList,
         valueList
       })
     },
-    // 当imageList 变化 赋值value
-    // changeValueArr(){
-    //   let valueList = this.data.imageList.map(item=>{
-    //     return item.path
-    //   })
-    //   this.setData({
-    //     valueList
-    //   })
-    // },
-    //侦听valueList图片数量变化触发函数
-    // changeImgArr(e) {
-    //   this.triggerEvent('setImg', {
-    //     imageList: this.data.valueList
-    //   })
-    // },
+    //侦听图片数量变化触发函数
+    changeImgArr(e) {
+      let valueList = this.data.valueList
+      let value = []
+      for(let i = 0 ; i < valueList.length ; i++){
+        if(valueList[i].length !== 0){
+          value.push(valueList[i])
+        }
+      }
+      this.triggerEvent('setImg', {
+        imgList: value
+      })
+    },
     //侦听上传事件 触发disabled事件
     changeDisabled(e) {
       this.triggerEvent('setDisabled', {
